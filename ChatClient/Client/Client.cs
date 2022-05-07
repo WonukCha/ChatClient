2339174f49fc2f931611392cdcd360a84c93b7c3
WonukCha <42984816+WonukCha@users.noreply.github.com>
2022-05-07 12:40:07 +0900

    220507 1. clint 구현 2. GUI 배경 구현

---
+++ ChatClient/Client/Client.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+  
+
+    internal class ChatClient
+    {
+        private TcpClient tcpClient = null;
+        private NetworkStream stream = null;
+        private bool run = false;
+        private Thread receiveThread = null;
+        private Thread sendThread = null;
+
+        private const int MAX_QUEUE_SIZE = 100;
+        private const int MAX_BUFFER_SIZE = 1024;
+
+        private ConcurrentQueue<byte[]> receiveQueue = new ConcurrentQueue<byte[]>();
+        private ConcurrentQueue<byte[]> sendQueue = new ConcurrentQueue<byte[]>();
+
+        public ChatClient()
+        {
+
+        }
+        ~ChatClient()
+        {
+            StopThread();
+        }
+        public bool Connect(string ip, int port)
+        {
+            //`if (IsTcpPortAvailable(port) == false)
+            //`    return false;
+
+            if (tcpClient != null)
+            {
+                tcpClient.Close();
+                tcpClient = null;
+            }
+
+            tcpClient = new TcpClient(ip, port);
+            stream = tcpClient.GetStream();
+            StartThread();
+            return true;
+        }
+        public void Disonnect()
+        {
+            if (tcpClient != null)
+            {
+                StopThread();
+                stream.Close();
+                tcpClient.Close();
+                tcpClient = null;
+            }
+        }
+        public bool SendData(byte[] data)
+        {
+            if(sendQueue.Count() > MAX_QUEUE_SIZE)
+            {
+                return false;
+            }
+            else
+            {
+                sendQueue.Enqueue(data);
+            }
+            return true;
+        }
+        public bool SendData(string str)
+        {
+            byte[] strByte = Encoding.UTF8.GetBytes(str);
+            return SendData(strByte);
+
+        }
+        public byte[] ReadData()
+        {
+            byte[] data = null;
+            while (receiveQueue.Count() > 0 && data == null)
+            {
+                receiveQueue.TryDequeue(out data);
+            }
+            return data;
+        }
+        private void StopThread()
+        {
+            run = false;
+            if (receiveThread != null)
+            {
+                receiveThread.Join();
+            }
+            if (sendThread != null)
+            {
+                sendThread.Join();
+            }
+        }
+        private void StartThread()
+        {
+            StopThread();
+            if(tcpClient != null)
+            {
+                if(tcpClient.Connected)
+                {
+                    run = true;
+                    sendThread = new Thread(SendThreadProc);
+                    sendThread.Name = "SendThread";
+                    receiveThread = new Thread(ReceiveThreadProc);
+                    receiveThread.Name = "ReceiveThread";
+
+                    sendThread.Start();
+                    receiveThread.Start();
+                }
+            }
+        }
+        private void ReceiveThreadProc()
+        {
+            int nbytes = 0;
+            byte[] outbuf = new byte[MAX_BUFFER_SIZE];
+            while (run)
+            {
+                nbytes = stream.Read(outbuf, 0, outbuf.Length);
+                if (nbytes == 0)
+                {
+                    //TODO : StopSend 분리
+                    run = false;
+                    if (sendThread != null)
+                    {
+                        sendThread.Join();
+                    }
+                    break;
+                }
+                else
+                {
+                    if (receiveQueue.Count() < MAX_QUEUE_SIZE)
+                    {
+                        receiveQueue.Enqueue(outbuf);
+                    }
+                }
+            }
+        }
+        private void SendThreadProc()
+        {
+            while (run)
+            {
+                if(sendQueue.Count() > 0)
+                {
+                    byte[] buf = null;
+                    sendQueue.TryDequeue(out buf);
+                    stream.Write(buf,0, buf.Length);
+                }
+                else
+                {
+                    Thread.Sleep(16);
+                }
+            }
+        }
+        private bool IsTcpPortAvailable(int tcpPort)
+        {
+            var ipgp = IPGlobalProperties.GetIPGlobalProperties();
+
+            // Check ActiveConnection ports
+            TcpConnectionInformation[] conns = ipgp.GetActiveTcpConnections();
+            foreach (var cn in conns)
+            {
+                if (cn.LocalEndPoint.Port == tcpPort)
+                {
+                    return false;
+                }
+            }
+
+            // Check LISTENING ports
+            IPEndPoint[] endpoints = ipgp.GetActiveTcpListeners();
+            foreach (var ep in endpoints)
+            {
+                if (ep.Port == tcpPort)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ChatClient/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ChatClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.pictureBoxClose = new System.Windows.Forms.PictureBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelTitle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.LightCoral;
+            this.panelTitle.Controls.Add(this.pictureBoxClose);
+            this.panelTitle.Controls.Add(this.labelTitle);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(1200, 50);
+            this.panelTitle.TabIndex = 0;
+            this.panelTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelTitle_MouseMove);
+            // 
+            // pictureBoxClose
+            // 
+            this.pictureBoxClose.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxClose.Image")));
+            this.pictureBoxClose.Location = new System.Drawing.Point(1146, 9);
+            this.pictureBoxClose.Name = "pictureBoxClose";
+            this.pictureBoxClose.Size = new System.Drawing.Size(42, 35);
+            this.pictureBoxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxClose.TabIndex = 1;
+            this.pictureBoxClose.TabStop = false;
+            this.pictureBoxClose.Click += new System.EventHandler(this.pictureBoxClose_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(12, 2);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(183, 45);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Chat Client";
+            // 
+            // panelMain
+            // 
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelMain.Location = new System.Drawing.Point(0, 50);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(1200, 800);
+            this.panelMain.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1200, 850);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "ChatClient";
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.PictureBox pictureBoxClose;
+    }
+}
+

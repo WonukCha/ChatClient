47588c8fae3c617047b5bf75d4603b29ff2b3ff3
WonukCha <cwu5529@gmail.com>
2022-05-19 20:56:35 +0900

    220519 1. 패킷 직렬화 구현

--- ChatClient/Client/Packet.cs
+++ ChatClient/Client/Packet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,18 +21,64 @@ namespace ChatClient.Client.Packet
 	//Todo : Set,Get, FromByte, ToByte
 	class LoginRequest
 	{
-		char[] id;
-		char[] pw;
+		byte[] id = new byte[PacketDefine.ID_SIZE];
+		byte[] pw = new byte[PacketDefine.PW_SIZE];
+
+		public bool SetIdPw(string id, string pw)
+        {
+			bool bResult = false;
+			if(id.Length <= PacketDefine.ID_SIZE && pw.Length <= PacketDefine.PW_SIZE)
+            {
+				Encoding.UTF8.GetBytes(id).CopyTo(this.id, 0);
+				Encoding.UTF8.GetBytes(id).CopyTo(this.pw, 0);
+				bResult = true;
+			}
+			return bResult;
+		}
+
+		public byte[] ToBytes()
+        {
+			List<byte> data = new List<byte>();
+			data.AddRange(this.id);
+			data.AddRange(this.pw);
+			return data.ToArray();
+		}
+
+
 	};
 	class LoginResponse
 	{
 		Int16 result;
+
+		public void FromBytes(byte[] bodyData,int offset)
+        {
+			result = BitConverter.ToInt16(bodyData,	offset);
+        }
 	};
 
 	class AllUserChatRequest
 	{
-		char[] checkCode;
-		char[] msg;
+		byte[] checkCode = new byte[PacketDefine.NAME_SIZE];
+		byte[] msg = new byte[PacketDefine.CHAT_SIZE];
+
+		public bool SetIdPw(string checkCode, string msg)
+		{
+			bool bResult = false;
+			if (checkCode.Length <= PacketDefine.NAME_SIZE && msg.Length <= PacketDefine.CHAT_SIZE)
+			{
+				Encoding.UTF8.GetBytes(checkCode).CopyTo(this.checkCode, 0);
+				Encoding.UTF8.GetBytes(msg).CopyTo(this.msg, 0);
+				bResult = true;
+			}
+			return bResult;
+		}
+		public byte[] ToBytes()
+		{
+			List<byte> data = new List<byte>();
+			data.AddRange(this.checkCode);
+			data.AddRange(this.msg);
+			return data.ToArray();
+		}
 	}
 	class AllUserChatResponse
 	{
@@ -41,36 +88,100 @@ namespace ChatClient.Client.Packet
 	class RoomEnterRequest
 	{
 		UInt16 roomNumber;
+
+		public bool SetRoomNumber(UInt16 roomNumber)
+        {
+			this.roomNumber = roomNumber;
+			return true;
+		}
+		public byte[] ToBytes()
+		{
+			List<byte> data = new List<byte>();
+			data.AddRange(BitConverter.GetBytes(roomNumber));
+			return data.ToArray();
+		}
+
 	};
 
 	class RoomEnterResponse
 	{
 		Int16 result = 0;
+		public void FromBytes(byte[] bodyData, int offset)
+		{
+			result = BitConverter.ToInt16(bodyData, offset);
+		}
 	};
 
 	class RoomLeaveRequest
 	{
 		UInt16 roomNumber;
+		public bool SetRoomNumber(UInt16 roomNumber)
+		{
+			this.roomNumber = roomNumber;
+			return true;
+		}
+		public byte[] ToBytes()
+		{
+			List<byte> data = new List<byte>();
+			data.AddRange(BitConverter.GetBytes(roomNumber));
+			return data.ToArray();
+		}
 	};
 
 	class RoomLeaveResponse
 	{
 		Int16 result = 0;
+		public void FromBytes(byte[] bodyData, int offset)
+		{
+			result = BitConverter.ToInt16(bodyData, offset);
+		}
 	};
 
 	class RoomChatRequest
 	{
-		char[] msg;
+		byte[] msg = new byte[PacketDefine.CHAT_SIZE];
+		public bool SetValue(string msg)
+		{
+			bool bResult = false;
+			if (msg.Length <= PacketDefine.CHAT_SIZE)
+			{
+				Encoding.UTF8.GetBytes(msg).CopyTo(this.msg, 0);
+				bResult = true;
+			}
+			return bResult;
+		}
+		public byte[] ToBytes()
+		{
+			List<byte> data = new List<byte>();
+			data.AddRange(msg);
+			return data.ToArray();
+		}
 	};
 
 	class RoomChatResponse
 	{
 		Int16 result = 0;
+		public void FromBytes(byte[] bodyData, int offset)
+		{
+			result = BitConverter.ToInt16(bodyData, offset);
+		}
 	};
 
 	class RoomChatNotify
 	{
-		char[] id;
-		char[] msg;
+		byte[] id = new byte[PacketDefine.ID_SIZE];
+		byte[] msg = new byte[PacketDefine.CHAT_SIZE];
+		public void SetValue(string id, string msg)
+		{
+			Encoding.UTF8.GetBytes(id).CopyTo(this.id, 0);
+			Encoding.UTF8.GetBytes(msg).CopyTo(this.msg, 0);
+		}
+		public byte[] ToBytes()
+		{
+			List<byte> data = new List<byte>();
+			data.AddRange(id);
+			data.AddRange(msg);
+			return data.ToArray();
+		}
 	};
 }
